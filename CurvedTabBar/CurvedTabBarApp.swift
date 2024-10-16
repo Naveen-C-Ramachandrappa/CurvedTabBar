f81577463ecb9a43ecf206e247e5ae6052e55a8e
NaveenRam <accessnaveen@gmail.com>
2024-10-16 11:10:38 -0500

    Initial Commit

---
+++ CurvedTabBar/CurvedTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurvedTabBarApp.swift
+//  CurvedTabBar
+//
+//  Created by Naveen Ramachandrappa on 10/16/24.
+//
+
+import SwiftUI
+
+@main
+struct CurvedTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
